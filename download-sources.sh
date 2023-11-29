8e232e7c9bbb8dbe950189654ee8f6ba77fddc4c
Juan Llamas <38849891+xoltia@users.noreply.github.com>
2023-11-29 09:24:34 -0600

    Add anidb-titles.dat to download script

--- download-sources.sh
+++ download-sources.sh
@@ -1,3 +1,10 @@
 echo "Downloading anime-offline-database-minified.json"
 wget -P ./data/ https://raw.githubusercontent.com/manami-project/anime-offline-database/master/anime-offline-database-minified.json
+
+echo "Downloading anidb-titles.dat"
+wget --user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:21.0) Gecko/20100101 Firefox/21.0" \
+     -O - https://anidb.net/api/anime-titles.dat.gz \
+     | gunzip \
+     | sort > ./data/anidb-titles.dat
+
 echo "Done!"
